70fd777c57412531c1dffe9df19d5ab719122591
Hariram6674 <121010395+Hariram6674@users.noreply.github.com>
2022-12-19 17:21:36 +0530

    Update ascending&descending.py

--- ascending&descending.py
+++ ascending&descending.py
@@ -1,3 +1,4 @@
+#for ascending
 n=100000
 fp = open("ascending_100000.txt","w")
 fp.write(str(n)+" ")  
@@ -6,6 +7,7 @@ for i in range(1,n):
 fp.write(str(n))
 fp.close()
 
+#for descending
 n=1000000
 fp = open("descending_1000000.txt","w")
 fp.write(str(n)+" ")  
